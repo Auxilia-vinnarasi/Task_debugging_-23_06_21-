a0db5b9801fd0620e2a54832a82a1dacb60fe009
Auxilia-vinnarasi <83941036+Auxilia-vinnarasi@users.noreply.github.com>
2021-06-28 08:55:16 +0530

    Add files via upload

---
+++ title_caps.js
@@ -0,0 +1,16 @@
+var arro = ["guvi", "geek", "zen", "fullstack"];
+var ano = function(arro)
+{
+ for (var i = 0; i < arro.length; i++)
+ {
+ console.log(arro[i][0].toUpperCase() + arro[i].substr(1));
+ }
+}
+ano(arro);
+
+/*output:
+Guvi
+Geek
+Zen
+Fullstack*/
+
